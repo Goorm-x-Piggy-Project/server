9db14ab6ed6b352cf31d7677d8f2252ad00a8437
jieun <ju3391@naver.com>
2024-12-17 18:11:03 +0900

    fix(AccountServiceClient): 어노테이션 및 코드 수정
    - RequestMapping 방식에서 GetMapping으로 변경

--- notification-service/src/main/java/com/piggymetrics/notification/client/AccountServiceClient.java
+++ notification-service/src/main/java/com/piggymetrics/notification/client/AccountServiceClient.java
@@ -1,15 +1,16 @@
 package com.piggymetrics.notification.client;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "account-service")
 public interface AccountServiceClient {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/accounts/{accountName}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	// GET 요청
+	@GetMapping(value = "/accounts/{accountName}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	String getAccount(@PathVariable("accountName") String accountName);
 
 }
