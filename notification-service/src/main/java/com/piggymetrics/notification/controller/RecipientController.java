060e5edcff36d4955f9fc506d24a1234d340c76c
jieun <ju3391@naver.com>
2024-12-17 17:45:19 +0900

    fix(RecipientController): 의존성 변경
    - javax → jakarta로 변경
    - 생성자 주입 방식으로 변경
    - RequestMapping 방식에서 GetMapping과 PutMapping으로 변경

--- notification-service/src/main/java/com/piggymetrics/notification/controller/RecipientController.java
+++ notification-service/src/main/java/com/piggymetrics/notification/controller/RecipientController.java
@@ -3,28 +3,33 @@ package com.piggymetrics.notification.controller;
 import com.piggymetrics.notification.domain.Recipient;
 import com.piggymetrics.notification.service.RecipientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid; // javax → jakarta로 변경
 import java.security.Principal;
 
 @RestController
 @RequestMapping("/recipients")
 public class RecipientController {
 
-	@Autowired
-	private RecipientService recipientService;
+	private final RecipientService recipientService;
 
-	@RequestMapping(path = "/current", method = RequestMethod.GET)
+	// 생성자 주입 방식
+	@Autowired
+	public RecipientController(RecipientService recipientService) {
+		this.recipientService = recipientService;
+	}
+
+	// GET 요청
+	@GetMapping("/current")
 	public Object getCurrentNotificationsSettings(Principal principal) {
 		return recipientService.findByAccountName(principal.getName());
 	}
 
-	@RequestMapping(path = "/current", method = RequestMethod.PUT)
-	public Object saveCurrentNotificationsSettings(Principal principal, @Valid @RequestBody Recipient recipient) {
+	// PUT 요청
+	@PutMapping("/current")
+	public Object saveCurrentNotificationsSettings(Principal principal,
+												   @Valid @RequestBody Recipient recipient) {
 		return recipientService.save(principal.getName(), recipient);
 	}
 }
