0c44793fd11f0793f8110213b648871af3787a65
JiinHong <com5942@seoultech.ac.kr>
2024-12-18 10:49:12 +0900

    Security Config 수정

--- config/src/main/java/com/piggymetrics/config/SecurityConfig.java
+++ config/src/main/java/com/piggymetrics/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.piggymetrics.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import static org.springframework.security.config.Customizer.withDefaults;
-
-/**
- * @author cdov
- */
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -16,10 +12,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults());
+                .httpBasic(httpBasic -> {});
+
         return http.build();
     }
 }
