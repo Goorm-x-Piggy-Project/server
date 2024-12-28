94b76e7a581ea1b8df92cdfbd8918064549426fb
YuyoungRhee <bbclara@naver.com>
2024-12-28 15:53:37 +0900

    fix(gateway): 프론트 - 환율 api 호출 주소 수정 #37
    - // -> 슬래시 중복 수정

--- gateway/src/main/resources/static/js/launch.js
+++ gateway/src/main/resources/static/js/launch.js
@@ -80,7 +80,7 @@ $(window).load(function(){
         global.mobileClient = true;
 	}
 
-    $.getJSON("api/v1//statistics/rates/default", function( data ) {
+    $.getJSON("api/v1/statistics/rates/default", function( data ) {
         global.krw = 1 / data.KRW;
         global.usd = 1 / data.USD;
     });
