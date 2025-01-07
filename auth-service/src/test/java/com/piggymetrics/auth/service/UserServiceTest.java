4296a27789ee7662809f31950bd1bc29fdedd397
ksah3756 <ksah3756@gmail.com>
2025-01-07 10:59:28 +0900

    테스트 코드 추가, UserCreateReqDto validation 추가

--- auth-service/src/test/java/com/piggymetrics/auth/service/UserServiceTest.java
+++ auth-service/src/test/java/com/piggymetrics/auth/service/UserServiceTest.java
@@ -1,49 +1,65 @@
-//package com.piggymetrics.auth.service;
-//
-//import com.piggymetrics.auth.domain.User;
-//import com.piggymetrics.auth.repository.UserRepository;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//
-//import java.util.Optional;
-//
-//import static org.mockito.Mockito.*;
-//import static org.mockito.MockitoAnnotations.initMocks;
-//
-//public class UserServiceTest {
-//
-//	@InjectMocks
-//	private UserService userService;
-//
-//	@Mock
-//	private UserRepository repository;
-////
-////	@Before
-////	public void setup() {
-////		initMocks(this);
-////	}
-//
-//	@Test
-//	public void shouldCreateUser() {
-//
-//		User user = new User();
-//		user.setUsername("name");
-//		user.setPassword("password");
-//
-//		userService.create(user);
-//		verify(repository, times(1)).save(user);
-//	}
-//
-//	@Test
-//	public void shouldFailWhenUserAlreadyExists() {
-//
-//		User user = new User();
-//		user.setUsername("name");
-//		user.setPassword("password");
-//
-//		when(repository.findById(user.getUsername())).thenReturn(Optional.of(new User()));
-//		Assertions.assertThatThrownBy(() -> userService.create(user)).isInstanceOf(IllegalArgumentException.class);
-//	}
-//}
+package com.piggymetrics.auth.service;
+
+import com.piggymetrics.auth.Exception.UserException;
+import com.piggymetrics.auth.Exception.UserException.UserAlreadyExistsException;
+import com.piggymetrics.auth.domain.User;
+import com.piggymetrics.auth.domain.dto.request.UserCreateReqDto;
+import com.piggymetrics.auth.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+	@Mock
+	private PasswordEncoder passwordEncoder;
+    @InjectMocks
+	private UserService userService;
+
+	@Test
+	@DisplayName("사용자 생성 시 repository.save() 메소드가 호출된다.")
+	public void shouldCreateUser() {
+		// given
+        UserCreateReqDto userCreateReqDto = UserCreateReqDto.builder()
+                .username("name")
+                .password("password")
+                .build();
+
+		// when
+		userService.create(userCreateReqDto);
+
+		// then
+		verify(repository, times(1)).save(any(User.class));
+	}
+
+	@Test
+	@DisplayName("사용자 생성 시 이미 존재하는 사용자가 있으면 UserAlreadyExistsException이 발생한다.")
+	public void shouldFailWhenUserAlreadyExists() {
+		// given
+		User user = User.of("name", "password");
+		UserCreateReqDto userCreateReqDto = UserCreateReqDto.builder()
+				.username(user.getUsername())
+				.password(user.getPassword())
+				.build();
+
+		// when
+		when(repository.findById(user.getUsername())).thenReturn(Optional.of(user));
+
+		// then
+		assertThatThrownBy(() ->
+				userService.create(userCreateReqDto)).isInstanceOf(UserAlreadyExistsException.class);
+	}
+}
--- auth-service/src/test/java/com/piggymetrics/auth/service/security/MongoUserDetailsServiceTest.java
+++ auth-service/src/test/java/com/piggymetrics/auth/service/security/MongoUserDetailsServiceTest.java
@@ -1,47 +1,49 @@
-//package com.piggymetrics.auth.service.security;
-//
-//import com.piggymetrics.auth.domain.User;
-//import com.piggymetrics.auth.repository.UserRepository;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//
-//import java.util.Optional;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.Matchers.any;
-//import static org.mockito.Mockito.when;
-//import static org.mockito.MockitoAnnotations.initMocks;
-//
-//public class MongoUserDetailsServiceTest {
-//
-//	@InjectMocks
-//	private MongoUserDetailsService service;
-//
-//	@Mock
-//	private UserRepository repository;
-//
-//	@Before
-//	public void setup() {
-//		initMocks(this);
-//	}
-//
-//	@Test
-//	public void shouldLoadByUsernameWhenUserExists() {
-//
-//		final User user = new User();
-//
-//		when(repository.findById(any())).thenReturn(Optional.of(user));
-//		UserDetails loaded = service.loadUserByUsername("name");
-//
-//		assertEquals(user, loaded);
-//	}
-//
-//	@Test(expected = UsernameNotFoundException.class)
-//	public void shouldFailToLoadByUsernameWhenUserNotExists() {
-//		service.loadUserByUsername("name");
-//	}
-//}
+package com.piggymetrics.auth.service.security;
+
+import com.piggymetrics.auth.domain.User;
+import com.piggymetrics.auth.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MongoUserDetailsServiceTest {
+
+	@Mock
+	private UserRepository repository;
+	@InjectMocks
+	private MongoUserDetailsService service;
+
+	@Test
+	@DisplayName("사용자 이름으로 사용자 정보를 로드한다.")
+	public void shouldLoadByUsernameWhenUserExists() {
+		// given
+		final User user = User.of("user", "password");
+		when(repository.findById(any())).thenReturn(Optional.of(user));
+
+		// when
+		UserDetails loaded = service.loadUserByUsername("user");
+
+		// then
+		assertEquals(user, loaded);
+	}
+
+	@Test
+	@DisplayName("사용자 이름으로 사용자 정보를 로드할 때 사용자가 존재하지 않으면 예외가 발생한다.")
+	public void shouldFailToLoadByUsernameWhenUserNotExists() {
+		Assertions.assertThatThrownBy(() ->
+				service.loadUserByUsername("name")).isInstanceOf(UsernameNotFoundException.class);
+	}
+}
