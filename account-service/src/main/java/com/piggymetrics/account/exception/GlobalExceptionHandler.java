47ed90c725947b834098f872e80bb1b0c2ec5962
wjd4204 <brian876373@gmail.com>
2025-01-08 15:58:47 +0900

    fix: Exception 핸들러 수

--- account-service/src/main/java/com/piggymetrics/account/exception/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/piggymetrics/account/exception/GlobalExceptionHandler.java
@@ -1,16 +1,27 @@
 package com.piggymetrics.account.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void processValidationError(IllegalArgumentException e) {
+        log.info("Returning HTTP 400 Bad Request", e);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
